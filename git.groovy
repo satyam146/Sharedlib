68b28ed8bcef8a589afb0e1666bc968c126d8b62
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-26 00:20:24 +0530

    Update and rename git.groovy to java_build.groovy

--- git.groovy
+++
@@ -1,7 +0,0 @@
-def call(){
-  echo "SCM Checkout"
-git([branches: [
-        [name: '*/master'], 
-    credentialsId: githublogin,
-     url: git@github.com:satyam146/Test-jenkins.git])
-}
---
+++ java_build.groovy
@@ -0,0 +1,4 @@
+def call(){
+ echo "build"
+  mvn clean install
+}
