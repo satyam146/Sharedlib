2dbf7a0857f9088134e5c78bc872b27d1f6743ea
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-25 22:50:29 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,15 +1,20 @@
 def buildFunction(){
     stage('Build') {
         steps {         
-            sh "mvn package"               
-        } post {
-              success {
-                 junit '**/target/surefire-reports/TEST-*.xml'
-                 archiveArtifacts 'target/*.jar'
-                // echo 'running hello Job'
-                // build job: 'hello' "Trigerring another job post build"
-                }
-            }
-        }
+            sh ''' 
+            mvn package '''
+        }
+    }
+}
+            
+//          post {
+//               success {
+//                  junit '**/target/surefire-reports/TEST-*.xml'
+//                  archiveArtifacts 'target/*.jar'
+//                 // echo 'running hello Job'
+//                 // build job: 'hello' "Trigerring another job post build"
+//                 }
+//             }
+//         }
 
   
