6f7243ff90c1e98147e1c7514a341df59af8a08b
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-25 23:52:26 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,4 +1,4 @@
 def call(){
   echo "SCM Checkout"
-  sh "git branch: 'master', credentialsId: 'githublogin', url: 'git@github.com:satyam146/test2.git'"
+  git branch: 'master', credentialsId: 'githublogin', url: 'git@github.com:satyam146/test2.git
 }
