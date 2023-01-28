2df0bf9707862ca908510415702693630fcd064e
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-29 00:06:34 +0530

    Update upload.groovy

--- vars/upload.groovy
+++ vars/upload.groovy
@@ -7,7 +7,7 @@ def call(){
                                     {
                                         "pattern": "/var/lib/jenkins/workspace/Test/target/*.war",
                                         "target": "demo/",
-                                        "buildNumber": env.BUILD_NUMBER
+                                        "buildNumber": "env.BUILD_NUMBER"
                                     }
                                 ]
                             }"""
