1d28c38e6084d593aaf8a48f8d40114b81874b79
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-29 02:11:47 +0530

    Update java_build.groovy

--- vars/java_build.groovy
+++ vars/java_build.groovy
@@ -1,7 +1,7 @@
 def call(){
  deploy adapters: 
   [tomcat9(credentialsId: 'tomcat',
-   path: '',
+ 
    url: 'http://192.168.29.127:9090')],
    contextPath: '/usr/share/tomcat/webapps/',
    onFailure: false,
