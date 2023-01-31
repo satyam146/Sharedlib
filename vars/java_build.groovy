d0677a460f4e4af4a522faf22775904b8741a8a5
satyam146 <82771699+satyam146@users.noreply.github.com>
2023-01-31 14:29:33 +0530

    Update java_build.groovy

--- vars/java_build.groovy
+++ vars/java_build.groovy
@@ -2,7 +2,7 @@ def call(){
  deploy adapters: 
   [tomcat9(credentialsId: 'tomcat',
  
-   url: 'http://3.108.62.0:8006')],
+   url: 'http://3.109.183.122')],
    contextPath: 'test',
    onFailure: false,
    war: '**/target/*.war' 
